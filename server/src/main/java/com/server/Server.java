c643f2752ec9e461dbc4eeee13ce6c96fcc5ef60
Antti Koponen <koponen942@outlook.com>
2023-02-12 01:10:10 +0200

    Catch some exceptions in main, get keystore and credentials from command-line args

--- server/src/main/java/com/server/Server.java
+++ server/src/main/java/com/server/Server.java
@@ -20,38 +20,48 @@ public class Server {
     private Server() {
     }
 
-    public static void main(String[] args) throws Exception {
-        //create the http server to port 8001 with default logger
-        HttpsServer server = HttpsServer.create(new InetSocketAddress(8001), 0);
-        SSLContext ssl = serverSSLContext();
+    public static void main(String[] args) {
+        try {
+            //create the http server to port 8001 with default logger
+            HttpsServer server = HttpsServer.create(new InetSocketAddress(8001), 0);
+            SSLContext ssl = serverSSLContext(args);
 
-        server.setHttpsConfigurator(new HttpsConfigurator(ssl) {
-            @Override
-            public void configure(HttpsParameters params) {
-                SSLContext c = getSSLContext();
-                SSLParameters sslParams = c.getDefaultSSLParameters();
-                params.setSSLParameters(sslParams);
-            }
-        });
+            server.setHttpsConfigurator(new HttpsConfigurator(ssl) {
+                @Override
+                public void configure(HttpsParameters params) {
+                    SSLContext c = getSSLContext();
+                    SSLParameters sslParams = c.getDefaultSSLParameters();
+                    params.setSSLParameters(sslParams);
+                }
+            });
 
-        UserAuthenticator auth = new UserAuthenticator();
-        server.createContext("/warning", new WarningHandler()).setAuthenticator(auth);
-        server.createContext("/registration", new RegistrationHandler(auth));
+            UserAuthenticator auth = new UserAuthenticator();
+            server.createContext("/warning", new WarningHandler()).setAuthenticator(auth);
+            server.createContext("/registration", new RegistrationHandler(auth));
 
-        server.start();
-        System.out.println("Started web server on " + server.getAddress());
+            server.start();
+            System.out.println("Started web server on " + server.getAddress());
+        } catch (FileNotFoundException e) {
+            System.err.println("Keystore file not found in " + System.getProperty("user.dir"));
+        } catch (Exception e) {
+            System.err.println("Error starting server!");
+            e.printStackTrace();
+        }
     }
 
-    private static SSLContext serverSSLContext() throws Exception {
-        char[] passphrase = "password".toCharArray();
-        KeyStore ks = KeyStore.getInstance("JKS");
-        final String keyStoreFile = "keystore.jks";
-        try {
-            ks.load(new FileInputStream(keyStoreFile), passphrase);
-        } catch (FileNotFoundException e) {
-            System.err.println("Keystore file " + keyStoreFile + " not found in " + System.getProperty("user.dir") + "!");
-            System.exit(1);
+    private static SSLContext serverSSLContext(String[] args) throws Exception {
+        final char[] passphrase;
+        final String keyStoreFile;
+        if (args.length == 2) {
+            keyStoreFile = args[0];
+            passphrase = args[1].toCharArray();
+        } else {
+            keyStoreFile = "keystore.jks";
+            passphrase = "password2".toCharArray();
         }
+
+        KeyStore ks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream(keyStoreFile), passphrase);
 
         KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
         kmf.init(ks, passphrase);
